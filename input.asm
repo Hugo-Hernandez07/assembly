63fc12e1f3b3c8b447ad771a4e89170ac9aa9935
Hugo-Hernandez07 <90530151+Hugo-Hernandez07@users.noreply.github.com>
2024-05-21 23:38:06 -0600

    Create input.asm

---
+++ input.asm
@@ -0,0 +1,3 @@
+LD A, 0x0F    ; Carga el valor hexadecimal 0x0F en el registro A
+LD B, A       ; Carga el valor de A en el registro B
+LD C, 0x55    ; Carga el valor hexadecimal 0x55 en el registro C
